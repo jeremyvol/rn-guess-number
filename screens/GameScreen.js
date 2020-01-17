5980182bead4c338c6bd5a2f6a59ee0e14e497a8
Jeremy VOLLANT <j.vollant@gmail.com>
2020-01-17 14:50:50 +0100

    guess number wip

---
+++ screens/GameScreen.js
@@ -0,0 +1,80 @@
+import React, { useState, useRef } from 'react';
+import { View, Text, Button, StyleSheet, Alert } from 'react-native';
+
+import NumberContainer from '../components/NumberContainer';
+import Card from '../components/Card';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = props => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+
+  const currentMin = useRef(1);
+  const currentMax = useRef(100);
+
+  const nextGuessHandler = direction => {
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie", 'You know that this is wrong...', [
+        { text: 'sorry', style: 'cancel' }
+      ]);
+      return;
+    }
+    if (direction === 'lower') {
+      currentMax.current = currentGuess;
+    } else {
+      currentMin.current = currentGuess;
+    }
+    const nextNumber = generateRandomBetween(
+      currentMin.current,
+      currentMax.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button
+          title="LOWER"
+          onPress={nextGuessHandler.bind(this, 'lower')}></Button>
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, 'greater')}></Button>
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%'
+  }
+});
+
+export default GameScreen;
